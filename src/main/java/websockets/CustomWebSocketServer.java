21c027a5a6c24b2083de5c6190f3b335fb1095ac
Maxim Subotin <maxim.sub21@gmail.com>
2023-12-08 09:43:05 +0200

    fixing some tabulation

--- src/main/java/websockets/CustomWebSocketServer.java
+++ src/main/java/websockets/CustomWebSocketServer.java
@@ -50,150 +50,148 @@ import util.MessageAdapter;
  */
 public class CustomWebSocketServer extends WebSocketServer {
 
-  private Map<WebSocket, String> connectedUsersMap;
-  private Map<String, WebSocket> reverseLookupMap;
-  private final Gson gson = new GsonBuilder().registerTypeAdapter(Message.class, new MessageAdapter()).create();
+    private Map<WebSocket, String> connectedUsersMap;
+    private Map<String, WebSocket> reverseLookupMap;
+    private final Gson gson = new GsonBuilder().registerTypeAdapter(Message.class, new MessageAdapter()).create();
 
-  public CustomWebSocketServer(int port, Draft_6455 draft) {
-    super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
-    connectedUsersMap = new ConcurrentHashMap<>();
-    reverseLookupMap = new ConcurrentHashMap<>();
-  }
-
-  @Override
-  public void onOpen(WebSocket conn, ClientHandshake handshake) {
-    // Extract the session token from the WebSocket client query
-    String sessionToken = extractSessionFromQuery(handshake.getResourceDescriptor());
-
-    // Look up the associated username from the session token (your database logic here)
-    String userId = getUserIdFormSession(sessionToken);
-
-    if (userId != null && !connectedUsersMap.containsValue(userId)) {
-      // Authentication is successful, proceed with WebSocket communication
-      connectedUsersMap.put(conn, userId);
-      reverseLookupMap.put(userId, conn);
-
-      // Create a system message
-      Message sysMessage = new Message("CONNECT//" + userId, userId, userId, new Timestamp(System.currentTimeMillis()).toString() ,"-1");
-      sysMessage.setIsSystemMessage(true);
-
-      // Serialize the message object to JSON
-      String jsonMessage = gson.toJson(sysMessage);
-
-      // Send the json system message
-      onMessage(conn,jsonMessage);
-      System.out.println(userId + " has entered the room!");
-    } else {
-      // Invalid session token, reject the WebSocket connection
-      System.out.println("ERROR!");
-      conn.closeConnection(401, "Unauthorized");
+    public CustomWebSocketServer(int port, Draft_6455 draft) {
+        super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
+        connectedUsersMap = new ConcurrentHashMap<>();
+        reverseLookupMap = new ConcurrentHashMap<>();
     }
-  }
 
-  @Override
-  public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-    // Get the associated username
-    String userId = connectedUsersMap.get(conn);
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        // Extract the session token from the WebSocket client query
+        String sessionToken = extractSessionFromQuery(handshake.getResourceDescriptor());
 
-    if (userId != null) {
-      // Create a system message
-      Message sysMessage = new Message("DISCONNECT//" + userId, userId, userId, new Timestamp(System.currentTimeMillis()).toString() ,"-1");
-      sysMessage.setIsSystemMessage(true);
+        // Look up the associated username from the session token (your database logic here)
+        String userId = getUserIdFormSession(sessionToken);
 
-      // Serialize the message object to JSON
-      String jsonMessage = gson.toJson(sysMessage);
+        if (userId != null && !connectedUsersMap.containsValue(userId)) {
+            // Authentication is successful, proceed with WebSocket communication
+            connectedUsersMap.put(conn, userId);
+            reverseLookupMap.put(userId, conn);
 
-      // Send the json system message
-      onMessage(conn,jsonMessage);
+            // Create a system message
+            Message sysMessage = new Message("CONNECT//" + userId, userId, userId, new Timestamp(System.currentTimeMillis()).toString() ,"-1");
+            sysMessage.setIsSystemMessage(true);
 
-      // Remove the connection from the map
-      connectedUsersMap.remove(conn);
-      reverseLookupMap.remove(userId);
+            // Serialize the message object to JSON
+            String jsonMessage = gson.toJson(sysMessage);
 
-      // Remove the user from the connected users table in the database
-      if (!Database.removeUserSession(userId))
-        System.out.println("Could not complete the user session removal form the database. Error in onClose Override function in CustomWebSocketServer.");
-
-      System.out.println(userId + " has left the room!");
+            // Send the json system message
+            onMessage(conn,jsonMessage);
+            System.out.println(userId + " has entered the room!");
+        } else {
+            // Invalid session token, reject the WebSocket connection
+            System.out.println("ERROR!");
+            conn.closeConnection(401, "Unauthorized");
+        }
     }
-  }
 
-  @Override
-  public void onMessage(WebSocket conn, String message) {
-    // Deserialize the received JSON string back to your custom object
-    Message receivedMessage = gson.fromJson(message, Message.class);
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        // Get the associated username
+        String userId = connectedUsersMap.get(conn);
 
-    if (receivedMessage.getIsSystemMessage()) { // it is a system message
-      broadcast(message);
+        if (userId != null) {
+            // Create a system message
+            Message sysMessage = new Message("DISCONNECT//" + userId, userId, userId, new Timestamp(System.currentTimeMillis()).toString() ,"-1");
+            sysMessage.setIsSystemMessage(true);
+
+            // Serialize the message object to JSON
+            String jsonMessage = gson.toJson(sysMessage);
+
+            // Send the json system message
+            onMessage(conn,jsonMessage);
+
+            // Remove the connection from the map
+            connectedUsersMap.remove(conn);
+            reverseLookupMap.remove(userId);
+
+            // Remove the user from the connected users table in the database
+            if (!Database.removeUserSession(userId))
+                System.out.println("Could not complete the user session removal form the database. Error in onClose Override function in CustomWebSocketServer.");
+
+            System.out.println(userId + " has left the room!");
+        }
     }
-    else {
-      // Access the receiver and send him the message. If he is disconnected then don't send him a message.
-      String receiverUserId = receivedMessage.getReceiver();
-      if (connectedUsersMap.containsValue(receiverUserId)) {
-        WebSocket Receiver = reverseLookupMap.get(receiverUserId);
-        Receiver.send(message);
-      }
-      // Using send method to show the message on the sender's screen
-      conn.send(message);
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        // Deserialize the received JSON string back to your custom object
+        Message receivedMessage = gson.fromJson(message, Message.class);
+
+        if (receivedMessage.getIsSystemMessage()) { // it is a system message
+            broadcast(message);
+        }
+        else {
+            // Access the receiver and send him the message. If he is disconnected then don't send him a message.
+            String receiverUserId = receivedMessage.getReceiver();
+            if (connectedUsersMap.containsValue(receiverUserId)) {
+                WebSocket Receiver = reverseLookupMap.get(receiverUserId);
+                Receiver.send(message);
+            }
+            // Using send method to show the message on the sender's screen
+            conn.send(message);
+        }
     }
-  }
 
-  @Override
-  public void onError(WebSocket conn, Exception ex) {
-    ex.printStackTrace();
-    if (conn != null) {
-      // some errors like port binding failed may not be assignable to a specific websocket
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+        if (conn != null) {
+            // some errors like port binding failed may not be assignable to a specific websocket
+        }
     }
-  }
 
-  @Override
-  public void onStart() {
-    System.out.println("Server started!");
-    setConnectionLostTimeout(0);
-    setConnectionLostTimeout(100);
-  }
-
-
-  // Helper Functions  // // // // // // // // // // // // // // // //
-  private String extractSessionFromQuery(String query) {
-    try {
-      URI uri = new URI(query);
-      String sessionToken = uri.getQuery(); // Get the query portion
-      if (sessionToken != null && sessionToken.startsWith("session=")) {
-        return sessionToken.substring("session=".length());
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+    @Override
+    public void onStart() {
+        System.out.println("Server started!");
+        setConnectionLostTimeout(0);
+        setConnectionLostTimeout(100);
     }
-    return null; // Session token not found or invalid
-  }
 
-  private String getUserIdFormSession(String sessionToken) {
-    // Return the username or null if not found
-    return Database.getUserIdBySession(sessionToken); // returns null if not found
-  }
-  // // // // // // // // // // // // // // // // // // // // // // //
 
-  public static void main(String[] args) throws InterruptedException, IOException {
-    int port = 8888; // 843 flash policy port
-
-    CustomWebSocketServer s = new CustomWebSocketServer(port, new Draft_6455());
-    s.start();
-    System.out.println("ChatServer started on port: " + s.getPort());
-
-    BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
-    while (true) {
-      String in = sysin.readLine();
-      s.broadcast(in);
-      if (in.equals("q")) {
-        s.stop(1000);
-        break;
-      }
+    // Helper Functions  // // // // // // // // // // // // // // // //
+    private String extractSessionFromQuery(String query) {
+        try {
+            URI uri = new URI(query);
+            String sessionToken = uri.getQuery(); // Get the query portion
+            if (sessionToken != null && sessionToken.startsWith("session=")) {
+                return sessionToken.substring("session=".length());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null; // Session token not found or invalid
     }
-    if (!Database.cleanUserSessionTabel())
-      System.out.println("Could not properly clean user session table.");
-  }
 
+    private String getUserIdFormSession(String sessionToken) {
+        // Return the username or null if not found
+        return Database.getUserIdBySession(sessionToken); // returns null if not found
+    }
+    // // // // // // // // // // // // // // // // // // // // // // //
 
+    public static void main(String[] args) throws InterruptedException, IOException {
+        int port = 8888; // 843 flash policy port
+
+        CustomWebSocketServer s = new CustomWebSocketServer(port, new Draft_6455());
+        s.start();
+        System.out.println("ChatServer started on port: " + s.getPort());
+
+        BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String in = sysin.readLine();
+            s.broadcast(in);
+            if (in.equals("q")) {
+              s.stop(1000);
+              break;
+            }
+        }
+        if (!Database.cleanUserSessionTabel())
+            System.out.println("Could not properly clean user session table.");
+    }
 
 }
